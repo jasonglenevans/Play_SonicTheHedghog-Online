b472289dea4d0c7886da4f4a9f0b40e9597f70b8
jasonglenevans <56980043+jasonglenevans@users.noreply.github.com>
2022-03-09 15:52:53 -0500

    Upload from console

--- upload.sh
+++ upload.sh
@@ -3,4 +3,4 @@ git add .
 git commit -m "Upload from console"
 git branch -M main
 git remote add origin https://github.com/jasonglenevans/GvbvdxxChat.git
-git push -u origin main
+git push -f origin main
